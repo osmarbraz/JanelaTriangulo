8e9eb1f0a60edef4905430efa9b00e6ccf288e00
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2024-04-17 18:49:07 -0300

    Inicio do projeto

---
+++ src/main/java/com/janelatriangulo/Principal.java
@@ -0,0 +1,11 @@
+package com.janelatriangulo;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        //Instância a interface gráfica
+        FrmTriangulo janela = new FrmTriangulo();
+        //Deixa a janela visível
+        janela.setVisible(true);
+    }
+}
